f36828bf18982fadf090fd6bdb932100a1ab0230
cjb543 <cjb543@protonmail.com>
2025-03-03 21:19:21 -0800

    Finish Project Euler 1

--- ProjectEuler/1 - Multiples of 3 or 5/1.py
+++ ProjectEuler/1 - Multiples of 3 or 5/1.py
@@ -4,3 +4,11 @@ The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 be
 '''
 
 # Solution
+total = 0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total += i
+
+print(total)
+
+
