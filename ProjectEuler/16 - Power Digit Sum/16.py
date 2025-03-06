d51e065a3eaabdfe05e181ace4ea21ee0b199c50
cjb543 <cjb543@protonmail.com>
2025-03-06 14:57:33 -0800

    Finish Project Euler Problem 16

---
+++ ProjectEuler/16 - Power Digit Sum/16.py
@@ -0,0 +1,13 @@
+'''
+2615 = 32768 and the sum of its digits is 3+2+7+6+8=26
+
+What is the sum of the digits of the number 2^1000?
+'''
+def main():
+    x = 2 ** 1000
+    xString = str(x)
+    digit_sum = sum(int(digit) for digit in xString)
+    print(digit_sum)
+
+if __name__ == "__main__":
+    main()
