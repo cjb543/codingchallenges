22861c922e2a886e838e8f018b75b66c84d3f749
cjb543 <cjb543@protonmail.com>
2025-03-18 09:25:56 -0700

    Document 2024 3-1

--- AdventOfCode/2024/3 - Mull It Over/python/3-1.py
+++ AdventOfCode/2024/3 - Mull It Over/python/3-1.py
@@ -1,13 +1,20 @@
 import re
 def main():
+    # Open file
     with open("input.txt") as f:
+        # Read file into contents
         contents = f.read()
+        
+        # Create regex pattern to only search for "mul(#,#)"
         mul_string = r'mul\((-?\d+(\.\d+)?),(-?\d+(\.\d+)?)\)'
+
+        # Create list of all "mul(#,#)" instances
         matches = re.findall(mul_string,contents)
+
+        # Add to final sum and print
         final_sum = 0
         for match in matches:
             final_sum += int(match[0]) * int(match[2])
-        
         print(final_sum)
 
 if __name__ == "__main__":
